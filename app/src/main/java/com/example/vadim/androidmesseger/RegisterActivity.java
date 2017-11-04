7f02790dea48f5ee3a5e91757dea36773daae763
Vadim Denisov <vadim.denisov.a@yandex.by>
2017-11-04 18:01:26 +0200

    Created RegisterActivity

--- app/src/main/java/com/example/vadim/androidmesseger/RegisterActivity.java
+++ app/src/main/java/com/example/vadim/androidmesseger/RegisterActivity.java
@@ -1,31 +1,27 @@
 package com.example.vadim.androidmesseger;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Switch;
 
-public class AuthorizationActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText username, password;
-    private Button buttonLogin, buttonRegistration;
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText username, email, password;
+    private Button buttonConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authorization);
+        setContentView(R.layout.activity_register);
 
         username = findViewById(R.id.UsernameField);
+        email = findViewById(R.id.EmailField);
         password = findViewById(R.id.PasswordField);
 
-        buttonLogin = findViewById(R.id.LoginButton);
-        buttonRegistration = findViewById(R.id.RegistrationButton);
-
-        buttonLogin.setOnClickListener(this);
-        buttonRegistration.setOnClickListener(this);
+        buttonConfirm = findViewById(R.id.ConfirmButton);
+        buttonConfirm.setOnClickListener(this);
     }
 
     @Override
@@ -33,6 +29,7 @@ public class AuthorizationActivity extends AppCompatActivity implements View.OnC
         super.onSaveInstanceState(outState);
 
         outState.putString("username", username.getText().toString());
+        outState.putString("email", email.getText().toString());
         outState.putString("password", password.getText().toString());
     }
 
@@ -41,19 +38,16 @@ public class AuthorizationActivity extends AppCompatActivity implements View.OnC
         super.onRestoreInstanceState(savedState);
 
         username.setText(savedState.getString("username"));
+        email.setText(savedState.getString("email"));
         password.setText(savedState.getString("password"));
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-        case R.id.LoginButton:
-            // TODO Find user in DB, go to UserActivity
-            break;
-        case R.id.RegistrationButton:
-            Intent intent = new Intent(this, RegisterActivity.class);
-            startActivity(intent);
-            break;
+        switch (view.getId()) {
+            case R.id.ConfirmButton:
+                // TODO confirm rules. Added in DB
+                break;
         }
     }
 }
