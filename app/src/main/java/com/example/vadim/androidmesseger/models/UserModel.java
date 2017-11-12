d52d6a9d973d253da1bb1a55fc3f38b22a29af70
Vadim Denisov <vadim.denisov.a@yandex.by>
2017-11-12 17:53:40 +0300

    Added save to intent and load from intent methods

--- app/src/main/java/com/example/vadim/androidmesseger/models/UserModel.java
+++ app/src/main/java/com/example/vadim/androidmesseger/models/UserModel.java
@@ -1,24 +1,45 @@
 package com.example.vadim.androidmesseger.models;
 
+
+import android.content.Intent;
+import android.util.Log;
+
 /**
  * Created by Vadim Denisov on 11/11/17.
  */
 
 public class UserModel {
+    public static final String ID_KEY = "id";
+    public static final String USERNAME_KEY = "username";
+    public static final String EMAIL_KEY = "email";
+
+    long id;
     String username;
     String email;
 
-    public UserModel(String username, String email) {
+    public UserModel(long id, String username, String email) {
+        this.id = id;
         this.username = username;
         this.email = email;
     }
 
+    public UserModel(Intent intent) {
+        this.id = intent.getExtras().getLong(ID_KEY);
+        this.username =  intent.getExtras().getString(USERNAME_KEY);
+        this.email = intent.getExtras().getString(EMAIL_KEY);
+    }
+
+    public long getId() { return id; }
     public String getUsername() {
         return username;
     }
-
     public String getEmail() {
         return email;
     }
 
+    public void putExtraUser(Intent intent) {
+        intent.putExtra(ID_KEY, id);
+        intent.putExtra(USERNAME_KEY, username);
+        intent.putExtra(EMAIL_KEY, email);
+    }
 }
