901e4ad5cd4e190a83f2b3289b18c8bb12189533
Vadim Denisov <vadim.denisov.a@yandex.by>
2017-11-21 21:24:43 +0300

    Added firebase to the project. Added internet permission

--- app/src/main/java/com/example/vadim/androidmesseger/fragments/UserInfoFragment.java
+++ app/src/main/java/com/example/vadim/androidmesseger/fragments/UserInfoFragment.java
@@ -2,7 +2,6 @@ package com.example.vadim.androidmesseger.fragments;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
