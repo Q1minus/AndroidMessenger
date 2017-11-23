30f389f408b6e45cf77c5cec415d0ca1f8d47069
Vadim Denisov <vadim.denisov.a@yandex.by>
2017-11-23 23:05:20 +0300

    Fixed MainActivity. Now does not stay in Back Stack. Removed activity_main.xml

--- app/src/main/java/com/example/vadim/androidmesseger/activities/MainActivity.java
+++ app/src/main/java/com/example/vadim/androidmesseger/activities/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        finish();
         startActivity(new Intent(this, SignInActivity.class));
     }
 }
