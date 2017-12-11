956be3d2be7dd6a0098c436cd36e2fc8ef92a8ba
Vadim Denisov <vadim.denisov.a@yandex.by>
2017-12-11 22:28:22 +0300

    Added loading friends photo from firebase storage

--- app/src/main/java/com/example/vadim/androidmesseger/adapters/UserAdapter.java
+++ app/src/main/java/com/example/vadim/androidmesseger/adapters/UserAdapter.java
@@ -1,19 +1,28 @@
 package com.example.vadim.androidmesseger.adapters;
 
 import android.app.Activity;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.vadim.androidmesseger.R;
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -33,11 +42,9 @@ public class UserAdapter extends FirebaseListAdapter<String> {
      * @param ref         The Firebase location to watch for data changes. Can also be a slice of a location,
      *                    using some combination of {@code limit()}, {@code startAt()}, and {@code endAt()}.
      */
-    ProgressBar progressBar;
 
-    public UserAdapter(Activity activity, Class<String> modelClass, int modelLayout, Query ref, ProgressBar progressBar) {
+    public UserAdapter(Activity activity, Class<String> modelClass, int modelLayout, Query ref) {
         super(activity, modelClass, modelLayout, ref);
-        this.progressBar = progressBar;
     }
 
     @Override
@@ -45,21 +52,37 @@ public class UserAdapter extends FirebaseListAdapter<String> {
         final TextView tv = view.findViewById(R.id.email);
         DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
 
-        progressBar.setVisibility(View.VISIBLE);
-        myRef.child("Users").child(model).child("email")
-                .addValueEventListener( new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        tv.setText(dataSnapshot.getValue(String.class));
-                        progressBar.setVisibility(View.GONE);
-                    }
+        myRef.child("Users").child(model).child("email").addValueEventListener( new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tv.setText(dataSnapshot.getValue(String.class));
+            }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) { }
-                });
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
 
-        tv.setText(model);
-    }
+
+        final ImageView iv = view.findViewById(R.id.photo);
+        final StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+
+        myRef.child("Users").child(model).child("photo").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String imageName = dataSnapshot.getValue(String.class);
+
+                if (imageName != null){
+                    Glide.with(mActivity)
+                            .using(new FirebaseImageLoader())
+                            .load(storageRef.child("images").child(imageName))
+                            .into(iv);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+}
 
     public ArrayList<String> getItems() {
         ArrayList<String> items = new ArrayList<>();
